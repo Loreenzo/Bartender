6d8b186833a07e94565e2531153c9aa4eb34e3b6
Lorenzo Jenkins <n00059769@ospreys.unf.edu>
2017-09-08 13:59:45 -0400

    INIT COMMIT
    Commiting for the first time

---
+++ Bartender/App_Data/Customers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[CustomerID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Phone] NVARCHAR(20) NULL, 
+    [Email] NVARCHAR(50) NULL
+)
